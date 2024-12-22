6be0b3b4236ec4a4959203aad8e76c349941e545
Peter Christian Würdemann <christian.wuerdemann@gmail.com>
2024-12-22 22:27:20 +0100

    fixed login bug

--- src/routes/(app)/login/+page.svelte
+++ src/routes/(app)/login/+page.svelte
@@ -1,13 +1,13 @@
 <script>
     import Button from "$lib/client/Button.svelte";
-    import Input from "$lib/client/Input.svelte";
+    import { inputStyles } from "$lib/client/styles";
 </script>
 
 <div class="flex justify-center py-8">
     <form method="POST" class="flex flex-col xl:w-[20%] w-[50%] gap-4">
-        <Input placeholder={'Email'} name={'email'} />
-        <Input type={'password'} placeholder={'Password'} name={'password'} />
-        <Button>
+        <input placeholder={'Email'} name={'email'} class={inputStyles} />
+        <input type={'password'} placeholder={'Password'} name={'password'} class={inputStyles} />
+        <Button type="submit">
             Log In
         </Button>
     </form>
