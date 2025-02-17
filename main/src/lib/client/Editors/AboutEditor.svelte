2963852012e05b2953b5d3ddae534353f18468b4
Peter Christian Würdemann <christian.wuerdemann@gmail.com>
2025-02-17 19:42:11 +0100

    added preview to about editor

--- main/src/lib/client/Editors/AboutEditor.svelte
+++ main/src/lib/client/Editors/AboutEditor.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
+    import Button from "../Button.svelte";
     export let value: string;
+    let isPreviewShown = false;
 </script>
 
 <div>
-    <h2>
-        About
-    </h2>
-    <textarea class="border-black border-[1px] rounded-xl min-h-[600px] w-full p-2" bind:value={value} />
+    <div class="flex justify-between">
+        <h2>
+            About
+        </h2>
+        <div class="flex gap-[2px]">
+            <Button handleClick={() => isPreviewShown = false}>
+                Edit
+            </Button>
+            <Button handleClick={() => isPreviewShown = true}>
+                Preview
+            </Button>
+        </div>
+    </div>
+    
+    {#if isPreviewShown}
+        <section id="about" class="flex flex-col justify-center gap-8 min-h-[600px] p-2">
+            {@html value}
+        </section>
+    {:else}
+        <textarea class="border-black border-[1px] rounded-xl min-h-[600px] w-full p-2" bind:value={value} />
+    {/if} 
+    
 </div>
